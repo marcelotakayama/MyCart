21b888049126f8273b20683b42ae41af23745547
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-09 17:03:36 -0300

    configurando os models

---
+++ MyCartAPI/MyCartAPI/Models/CartDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCartAPI.Models {
+    public class CartDetail {
+        public int IdProduct { get; set; }
+        public string NameProduct { get; set; }
+        public string CodeProduct { get; set; }
+        public string PriceProduct { get; set; }
+    }
+}
