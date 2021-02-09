13eac70d5e2d5a2a0b99663c0884d3032b9a4623
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-09 17:16:56 -0300

    configuracoes

---
+++ MyCartAPI/MyCartAPI/Models/CartDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCartAPI.Models {
+    public class CartDetailContext : DbContext {
+        public CartDetailContext(DbContextOptions<CartDetailContext> options) : base(options) {
+        }
+
+        public DbSet<CartDetail> CartDetails { get; set; }
+    }
+}
