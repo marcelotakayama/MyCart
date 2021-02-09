d2199cce5bf2cca5f5672eab3bf950970d084051
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-09 17:31:09 -0300

    subindo

--- MyCartAPI/MyCartAPI/Startup.cs
+++ MyCartAPI/MyCartAPI/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MyCartAPI.Models;
 
 namespace MyCartAPI {
     public class Startup {
@@ -27,6 +22,8 @@ namespace MyCartAPI {
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyCartAPI", Version = "v1" });
             });
+
+            services.AddDbContext<CartDetailContext>(opt => opt.UseSqlServer());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
