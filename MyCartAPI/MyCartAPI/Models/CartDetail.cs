5855b7fa6ec7902ea694aa54cc2b648067f334e4
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-10 09:51:54 -0300

    configurando os models com o dataanootation

--- MyCartAPI/MyCartAPI/Models/CartDetail.cs
+++ MyCartAPI/MyCartAPI/Models/CartDetail.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyCartAPI.Models {
     public class CartDetail {
+        [Key]
         public int IdProduct { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
         public string NameProduct { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
         public string CodeProduct { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
         public string PriceProduct { get; set; }
     }
 }
