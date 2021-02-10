ff6b48f662895e796ec48b48a0596cfeca76d3b9
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-10 11:06:46 -0300

    agr vai

---
+++ MyCartAPI/MyCartAPI/Controllers/CartDetailController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyCartAPI.Models;
+
+namespace MyCartAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartDetailController : ControllerBase
+    {
+        private readonly CartDetailContext _context;
+
+        public CartDetailController(CartDetailContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CartDetail
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CartDetail>>> GetCartDetails()
+        {
+            return await _context.CartDetails.ToListAsync();
+        }
+
+        // GET: api/CartDetail/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CartDetail>> GetCartDetail(int id)
+        {
+            var cartDetail = await _context.CartDetails.FindAsync(id);
+
+            if (cartDetail == null)
+            {
+                return NotFound();
+            }
+
+            return cartDetail;
+        }
+
+        // PUT: api/CartDetail/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCartDetail(int id, CartDetail cartDetail)
+        {
+            if (id != cartDetail.IdProduct)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cartDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CartDetail
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CartDetail>> PostCartDetail(CartDetail cartDetail)
+        {
+            _context.CartDetails.Add(cartDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCartDetail", new { id = cartDetail.IdProduct }, cartDetail);
+        }
+
+        // DELETE: api/CartDetail/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCartDetail(int id)
+        {
+            var cartDetail = await _context.CartDetails.FindAsync(id);
+            if (cartDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.CartDetails.Remove(cartDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CartDetailExists(int id)
+        {
+            return _context.CartDetails.Any(e => e.IdProduct == id);
+        }
+    }
+}
---
+++ MyCartAPI/MyCartAPI/Migrations/20210210140522_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyCartAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartDetails",
+                columns: table => new
+                {
+                    IdProduct = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameProduct = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CodeProduct = table.Column<string>(type: "nvarchar(10)", nullable: true),
+                    PriceProduct = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartDetails", x => x.IdProduct);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartDetails");
+        }
+    }
+}
